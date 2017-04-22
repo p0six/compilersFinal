8fe69e6b44c23b562e5e5fd718e0dd1304e8079d
Michael Romero <romerom@gmail.com>
2017-04-21 22:47:19 -0700

    refactored to use string buffer.. still have one ugly do nothing "continue"

--- src/com/rsh/cpsc323/Main.java
+++ src/com/rsh/cpsc323/Main.java
@@ -7,51 +7,94 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
-        String lineIn = new String(), lineOut = new String(), specialChars = "():;,)=*/+-";
-        Character c;
-        ArrayDeque<Character> ad = new ArrayDeque<Character>();
+    private static void partOne(BufferedReader br, BufferedWriter bw) throws IOException {
+        String lineIn, specialChars = "():;,)=*/+-";
+        Character c;int slashCount = 0, spaceCount = 0;
 
-        BufferedReader br = new BufferedReader(new FileReader(".//src//com//rsh//cpsc323//S2017.txt"));
-        BufferedWriter bw = new BufferedWriter(new FileWriter(".//src//com//rsh/cpsc323/finalv2.txt"));
-        int slashCount = 0, spaceCount = 0;
+        System.out.println("===================================");
+        System.out.println("| Part One Input:");
+        System.out.println("===================================");
+        StringBuilder sb = new StringBuilder();
         while (br.ready()) {
-            lineIn = br.readLine(); lineOut = "";
+            lineIn = br.readLine();
+            System.out.println(lineIn);
 
             for (int i = 0; i < lineIn.length(); i++) { // step 1: trim all whitespace...
-               c = lineIn.charAt(i);
-               if (c == '/') { // let's handle comments..
-                  slashCount++;
-                  if (slashCount == 4) { // comment began.. comment ended..
-                      slashCount = 0;
-                  }
-               } else if (slashCount >= 2 && slashCount < 4) { // ignores comments.. kinda ugly this way tbh...
-               } else { // we're not in a comment..
-                   if (!Character.isWhitespace(c)) {
-                       if (spaceCount == 0 && specialChars.indexOf(c) != -1) {
-                          lineOut += ' ';
-                          lineOut += c;
-                          lineOut += ' ';
-                          spaceCount++;
-                       } else {
-                           spaceCount = 0;
-                           lineOut += c;
-                       }
-                   } else {
-                       if (!lineOut.isEmpty() && spaceCount == 0) {
-                           spaceCount++;
-                           lineOut += ' ';
-                       }
-                   }
-               }
-            }
-            if (!lineOut.isEmpty()) {
-                if (lineOut.contains("PROGRAM")) { // sample output has an extra line after the line with PROGRAM
-                    lineOut += '\n';
+                c = lineIn.charAt(i);
+                if (c == '/') { // let's handle comments..
+                    slashCount++;
+                    if (slashCount == 4) { // comment began.. comment ended..
+                        slashCount = 0;
+                    }
+                } else if (slashCount >= 2 && slashCount < 4) { // ignores comments.. kinda ugly this way tbh...
+                    continue;
+                } else { // we're not in a comment..
+                    if (!Character.isWhitespace(c)) {
+                        if (spaceCount == 0 && specialChars.indexOf(c) != -1) {
+                            sb.append(" ");
+                            sb.append(c);
+                            sb.append(" ");
+                            spaceCount++;
+                        } else {
+                            spaceCount = 0;
+                            sb.append(c);
+                        }
+                    } else {
+                        if (sb.length() != 0 && spaceCount == 0) {
+                            spaceCount++;
+                            sb.append(" ");
+                        }
+                    }
                 }
-                bw.write(lineOut + '\n');
+            }
+            if (sb.length() != 0) {
+                //if (sb.toString().contains("PROGRAM")) {
+                    //lineOut += '\n';
+                    //sb.append('\n');
+                //} // rules say all blank lines must be removed, though it does not match sample output...
+                sb.append("\n");
+                System.out.println(sb.toString());
+                bw.write(sb.toString());
+                sb.delete(0, sb.length());
             }
         }
-        bw.close(); br.close();
+    }
+
+    private static boolean partTwo(BufferedReader br, BufferedWriter bw) throws IOException {
+        String lineIn, lineOut;
+        System.out.println("===================================");
+        System.out.println("| Part Two Input:");
+        System.out.println("===================================");
+        while (br.ready()) {
+            lineIn = br.readLine();
+            System.out.println(lineIn);
+        }
+        return true;
+    }
+
+    public static void main(String[] args) throws IOException {
+        // Part One
+        BufferedReader br = new BufferedReader(new FileReader(".//src//com//rsh//cpsc323//S2017.txt"));
+        BufferedWriter bw = new BufferedWriter(new FileWriter(".//src//com//rsh/cpsc323/finalv2.txt"));
+        partOne(br, bw); bw.close(); br.close();
+
+
+        // Part Two
+        BufferedReader br2 = new BufferedReader(new FileReader(".//src//com//rsh//cpsc323//finalv2.txt"));
+        BufferedWriter bw2 = new BufferedWriter(new FileWriter(".//src//com//rsh/cpsc323/finalv3.txt"));
+        if (partTwo(br2, bw2)) {
+            System.out.println("==================================");
+            System.out.println("| Accepted");
+            System.out.println("==================================");
+        } else {
+            System.out.println("==================================");
+            System.out.println("| Rejected");
+            System.out.println("==================================");
+            return;
+        } br2.close(); bw2.close();
+
+        // At this point... we need to translate the input into a final language...
+        BufferedReader br3 = new BufferedReader(new FileReader(".//src//com//rsh//cpsc323//finalv3.txt"));
+        System.out.println("made it here...");
     }
 }
